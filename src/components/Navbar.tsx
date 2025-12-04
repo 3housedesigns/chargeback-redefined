516d0b1bbd4d489ed10c9a29161599eaa0397709
3housedesigns <71693949+3housedesigns@users.noreply.github.com>
2025-12-04 14:44:18 -0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
           <a href="/" className="flex items-center gap-2">
             <div className="w-10 h-10 rounded-xl bg-primary/10 flex items-center justify-center">
               <img
-                src=*/Eye.png"
+                src="/Eye.png"
                 alt="Eye logo"
                 className="w-8 h-6 object-contain" 
               />
